35e1c473c96c3dd6688452536492ab51cb54251b
TsHristov <tsvetanhristov@yahoo.com>
2017-07-22 18:23:14 +0300

    More tests

--- Data-Structures/Dynamic-Array/main.cpp
+++ Data-Structures/Dynamic-Array/main.cpp
@@ -3,7 +3,7 @@
 
 void Test_CreateDynamicArray()
 {
-  std::cout << "Can create empty array...";
+  std::cout << "can create empty array...";
   DynamicArray<int> array;
   std::cout << "OK\n";
 }
@@ -26,11 +26,67 @@ void Test_GetIndexOfElement()
   std::cout << (condition1 && condition2 ? "OK\n" : "NOK\n");
 }
 
+void Test_CountElements()
+{
+  std::cout << "can count the elements with particular value...";
+  DynamicArray<int> array;
+  array.Append(1);
+  array.Append(1);
+  array.Append(1);
+  array.Append(2);
+  bool condition = array.Count(1) == 3;
+  std::cout << (condition ? "OK\n" : "NOK\n");
+}
+
+void Test_ReverseSequence()
+{
+  std::cout << "can reverse the sequence...";
+  DynamicArray<int> array;
+  for(int i = 0; i < 10; i++)
+  {
+    array.Append(i);
+  }
+  array.Reverse();
+  bool condition = true;
+  for(int i = 9, j = 0; i > 0 && j < 10; i--, j++)
+  {
+    if(array[j] != i) condition = false;
+  }
+  std::cout << (condition ? "OK\n" : "NOK\n");
+}
+
+void Test_Insert()
+{
+  std::cout << "can insert an element at given index...";
+  DynamicArray<int> array;
+  for(int i = 0; i < 5; i++)
+  {
+    array.Append(i);
+  }
+  array.Insert(1,666);
+  bool condition = array.Index(666) == 1;
+  std::cout << (condition ? "OK\n" : "NOK\n");
+}
+
+void Test_Empty()
+{
+  std::cout << "can check if container is empty...";
+  DynamicArray<int> array;
+  bool condition1 = array.IsEmpty();
+  array.Append(0);
+  bool condition2 = !array.IsEmpty();
+  std::cout << (condition1 && condition2 ? "OK\n" : "NOK\n");
+}
+
 int main()
 {
   std::cout << "Class DynamicArray: \n";
   Test_CreateDynamicArray();
   Test_AppendToArray();
   Test_GetIndexOfElement();
+  Test_CountElements();
+  Test_ReverseSequence();
+  Test_Insert();
+  Test_Empty();
   return 0;
 }
