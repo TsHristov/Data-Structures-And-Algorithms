3efdbd1de7dec16a9c82340a4f8219eda24278cd
TsHristov <tsvetanhristov@yahoo.com>
2017-07-29 19:22:47 +0300

    Add StackIterator and __eq__,__ne__ methods and more tests

--- Data-Structures/Stack-Dynamic-Array/Python/stack.py
+++ Data-Structures/Stack-Dynamic-Array/Python/stack.py
@@ -1,27 +1,55 @@
 class EmptyStack(Exception):
     pass
 
+
+class StackIterator:
+    """Iterates over the stack starting from top to bottom"""
+
+    def __init__(self, stack=[]):
+        self.__stack = stack
+        self.__current = len(stack)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.__current -= 1
+        if self.__current < 0:
+            raise StopIteration
+        else:
+            return self.__stack[self.__current]
+
+
 class Stack:
     def __init__(self):
-        self._stack = []
+        self.__stack = []
 
     def push(self, value):
-        self._stack.append(value)
+        self.__stack.append(value)
 
     def pop(self):
         if self.is_empty():
             raise EmptyStack("Stack is empty")
-        else:
-            return self._stack.pop()
+        return self.__stack.pop()
 
     def top(self):
         if self.is_empty():
             raise EmptyStack("Stack is empty")
-        else:
-            return self._stack[-1]
+        return self.__stack[-1]
 
     def is_empty(self):
-        return len(self._stack) == 0
+        return len(self.__stack) == 0
 
-    def len(self):
-        return len(self._stack)
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.__dict__ == other.__dict__
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __iter__(self):
+        return StackIterator(self.__stack)
+
+    def __len__(self):
+        return len(self.__stack)
--- Data-Structures/Stack-Dynamic-Array/Python/test.py
+++ Data-Structures/Stack-Dynamic-Array/Python/test.py
@@ -1,24 +1,60 @@
 import unittest
 import stack
 
-class TestStack(unittest.TestCase):
-    def setUp(self):
-        self._stack = stack.Stack()
 
+class TestStack(unittest.TestCase):
     def test_push(self):
-        pass
+        stack_test = stack.Stack()
+        stack_test.push(1)
+        stack_test.push(2)
+        self.assertTrue(not stack_test.is_empty())
 
     def test_pop(self):
-        pass
+        stack_test = stack.Stack()
+        stack_test.push(1)
+        stack_test.push(2)
+        self.assertEqual(stack_test.pop(), 2)
+        self.assertEqual(stack_test.pop(), 1)
 
     def test_top(self):
-        pass
+        stack_test = stack.Stack()
+        with self.assertRaises(stack.EmptyStack):
+            stack_test.top()
+        stack_test.push(1)
+        stack_test.push(2)
+        self.assertEqual(stack_test.top(), 2)
+        self.assertEqual(len(stack_test), 2)
 
     def test_is_empty(self):
-        pass
+        stack_test = stack.Stack()
+        self.assertTrue(stack_test.is_empty())
+        stack_test.push(1)
+        self.assertTrue(not stack_test.is_empty())
 
     def test_len(self):
-        pass
+        stack_test = stack.Stack()
+        self.assertEqual(len(stack_test), 0)
+        stack_test.push(1)
+        stack_test.push(2)
+        self.assertEqual(len(stack_test), 2)
+
+    def test_equal(self):
+        stack_test_1 = stack.Stack()
+        stack_test_2 = stack.Stack()
+        self.assertTrue(stack_test_1 == stack_test_1)
+        self.assertTrue(stack_test_2 == stack_test_2)
+        self.assertTrue(stack_test_1 == stack_test_2)
+        stack_test_1.push(1)
+        stack_test_1.push(2)
+        stack_test_2.push(1)
+        stack_test_2.push(2)
+        self.assertTrue(stack_test_1 == stack_test_2)
+
+    def test_not_equal(self):
+        stack_test_1 = stack.Stack()
+        stack_test_2 = stack.Stack()
+        stack_test_2.push(1)
+        self.assertTrue(stack_test_1 != stack_test_2)
 
 if __name__ == "__main__":
     unittest.main()
