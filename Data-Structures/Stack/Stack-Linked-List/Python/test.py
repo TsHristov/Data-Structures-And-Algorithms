eede9a2c08bf9deee9db11ba8b47c6783da89e46
TsHristov <tsvetanhristov@yahoo.com>
2017-07-30 17:14:18 +0300

    Add setUp() method to the unit tests

--- Data-Structures/Stack/Stack-Linked-List/Python/test.py
+++ Data-Structures/Stack/Stack-Linked-List/Python/test.py
@@ -3,40 +3,38 @@ import stack
 
 
 class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = stack.Stack()
+
     def test_push(self):
-        stack_test = stack.Stack()
-        stack_test.push(1)
-        stack_test.push(2)
-        self.assertTrue(not stack_test.is_empty())
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertFalse(self.stack.is_empty())
 
     def test_pop(self):
-        stack_test = stack.Stack()
-        stack_test.push(1)
-        stack_test.push(2)
-        self.assertEqual(stack_test.pop(), 2)
-        self.assertEqual(stack_test.pop(), 1)
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.pop(), 1)
 
     def test_top(self):
-        stack_test = stack.Stack()
         with self.assertRaises(stack.EmptyStack):
-            stack_test.top()
-        stack_test.push(1)
-        stack_test.push(2)
-        self.assertEqual(stack_test.top(), 2)
-        self.assertEqual(len(stack_test), 2)
+            self.stack.top()
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.top(), 2)
+        self.assertEqual(len(self.stack), 2)
 
     def test_is_empty(self):
-        stack_test = stack.Stack()
-        self.assertTrue(stack_test.is_empty())
-        stack_test.push(1)
-        self.assertTrue(not stack_test.is_empty())
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push(1)
+        self.assertFalse(self.stack.is_empty())
 
     def test_len(self):
-        stack_test = stack.Stack()
-        self.assertEqual(len(stack_test), 0)
-        stack_test.push(1)
-        stack_test.push(2)
-        self.assertEqual(len(stack_test), 2)
+        self.assertEqual(len(self.stack), 0)
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(len(self.stack), 2)
 
     def test_equal(self):
         pass
