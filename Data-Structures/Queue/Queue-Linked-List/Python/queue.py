ff5da951f0c19fbdcf62fead8ebfb8b1f7202241
TsHristov <tsvetanhristov@yahoo.com>
2017-07-30 12:26:51 +0300

    Add Queue iterator

--- Data-Structures/Queue/Queue-Linked-List/Python/queue.py
+++ Data-Structures/Queue/Queue-Linked-List/Python/queue.py
@@ -3,6 +3,22 @@ sys.path.insert(0, '../../../Linked-List/Python')
 import linked_list
 
 
+class QueueIterator:
+    def __init__(self, queue):
+        self._current = queue._first
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if not self._current:
+            raise StopIteration
+        else:
+            current = self._current
+            self._current = self._current.next
+            return current
+
+
 class EmptyQueue(Exception):
     pass
 
@@ -34,6 +50,9 @@ class Queue:
 
     def is_empty(self):
         return self._queue.is_empty()
+
+    def __iter__(self):
+        return QueueIterator(self._queue)
 
     def __len__(self):
         return self._queue.size()
