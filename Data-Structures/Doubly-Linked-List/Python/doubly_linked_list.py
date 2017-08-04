96151a8de659f3893dabb0f3bb416513cbf4a3a6
TsHristov <tsvetanhristov@yahoo.com>
2017-08-04 14:47:20 +0300

    Add insert_first, insert_last, first and last methods and unit tests

--- Data-Structures/Doubly-Linked-List/Python/doubly_linked_list.py
+++ Data-Structures/Doubly-Linked-List/Python/doubly_linked_list.py
@@ -4,9 +4,10 @@ import node
 class EmptyList(Exception):
     pass
 
+
 class DoublyLinkedList:
     def __init__(self):
-        self._header  = node.Node()
+        self._header = node.Node()
         self._trailer = node.Node()
         self._header._next = self._trailer
         self._trailer._previous = self._header
@@ -21,7 +22,7 @@ class DoublyLinkedList:
 
     def delete_node(self, node):
         if self.is_empty():
-            raise EmptyList()
+            raise EmptyList
         predecessor = node._previous
         successor = node._next
         predecessor._next = successor
@@ -30,6 +31,22 @@ class DoublyLinkedList:
         node._data = node._previous = node._next = None
         self._size -= 1
         return data
+
+    def insert_first(self, data):
+        self.insert_between(data, self._header, self._header._next)
+
+    def insert_last(self, data):
+        self.insert_between(data, self._trailer._previous, self._trailer)
+
+    def first(self):
+        if self.is_empty():
+            raise EmptyList
+        return self._header._next._data
+
+    def last(self):
+        if self.is_empty():
+            raise EmptyList
+        return self._trailer._previous._data
 
     def is_empty(self):
         return self._size == 0
---
+++ Data-Structures/Doubly-Linked-List/Python/test.py
@@ -0,0 +1,40 @@
+import unittest
+import doubly_linked_list
+
+
+class TestDoublyLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.list = doubly_linked_list.DoublyLinkedList()
+
+    def test_insert_first(self):
+        self.list.insert_first('a')
+        self.list.insert_first('b')
+        first = self.list._header._next._data
+        last = self.list._trailer._previous._data
+        self.assertEqual(first, 'b')
+        self.assertEqual(last, 'a')
+
+    def test_insert_last(self):
+        self.list.insert_last('a')
+        self.list.insert_last('b')
+        first = self.list._header._next._data
+        last = self.list._trailer._previous._data
+        self.assertEqual(first, 'a')
+        self.assertEqual(last, 'b')
+
+    def test_get_first(self):
+        with self.assertRaises(doubly_linked_list.EmptyList):
+            self.list.first()
+        self.list.insert_first('a')
+        self.list.insert_last('b')
+        self.assertEqual(self.list.first(), 'a')
+
+    def test_get_last(self):
+        with self.assertRaises(doubly_linked_list.EmptyList):
+            self.list.last()
+        self.list.insert_first('a')
+        self.list.insert_last('b')
+        self.assertEqual(self.list.last(), 'b')
+
+if __name__ == "__main__":
+    unittest.main()
